8a33cd85921a7f89071b6f9fea55c707228b802d
Harshit <rajputharshit48@gmail.com>
2024-06-24 22:48:52 +0530

    sdsadas

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
-'use client'
+"use client";
 
-import { Grid, Paper, Button, Box, styled, Card, CardActionArea, Typography, CardContent } from "@mui/material";
+import { Grid, Paper, Box, styled, Card, CardActionArea, Typography, CardContent } from "@mui/material";
 import Image from "next/image";
 import { useState, useRef } from "react";
 import fareimage from "../public/fareimage.jpg";
 import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 import RightBar from "@/components/Category/RightBar/RightBar";
 import AdvertisementBanner from "@/components/Category/RightBar/advertisementBanner";
-import CategoryCard from "@/components/Category/center/CategoryCard";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -19,100 +18,120 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Home() {
   const [spacing, setSpacing] = useState(2);
-  const scrollRef = useRef(null);
+  const mainContentRef = useRef(null);
+  const rightBarRef = useRef(null);
 
   const handleScroll = (direction) => {
-    if (scrollRef.current) {
-      const { scrollLeft, clientWidth } = scrollRef.current;
+    if (mainContentRef.current) {
+      const { scrollLeft, clientWidth } = mainContentRef.current;
       const scrollAmount = clientWidth / 2; // Adjust as needed
       if (direction === "next") {
-        scrollRef.current.scrollLeft = scrollLeft + scrollAmount;
+        mainContentRef.current.scrollLeft = scrollLeft + scrollAmount;
       } else {
-        scrollRef.current.scrollLeft = scrollLeft - scrollAmount;
+        mainContentRef.current.scrollLeft = scrollLeft - scrollAmount;
       }
     }
   };
 
   return (
-    <Box sx={{ p: 2 }}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
+    <div className="relative min-h-screen">
       <div className="flex items-center m-5 gap-5">
-      <div
-        className="cursor-pointer hover:bg-gray-200 h-[40px] w-[40px] flex items-center justify-center" // Apply hover background color directly
-        onClick={() => handleScroll("prev")}
-      >
-        <ArrowBackIos />
+        <div
+          className="cursor-pointer hover:bg-gray-200 h-[40px] w-[40px] flex items-center justify-center"
+          onClick={() => handleScroll("prev")}
+        >
+          <ArrowBackIos />
+        </div>
+        <div
+          style={{
+            overflowX: "auto",
+            whiteSpace: "nowrap",
+            overflowX: "hidden",
+            scrollBehavior: "smooth",
+          }}
+          ref={mainContentRef}
+        >
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+            <div
+              key={value}
+              style={{ display: "inline-block", marginRight: "5px" }}
+            >
+              <Image
+                src={fareimage}
+                alt="logo"
+                style={{ width: "350px", height: "500px" }}
+              />
+            </div>
+          ))}
+        </div>
+        <div
+          className="cursor-pointer hover:bg-gray-200 h-[40px] w-[40px] flex items-center justify-center"
+          onClick={() => handleScroll("next")}
+        >
+          <ArrowForwardIos />
+        </div>
       </div>
-      <div
-        style={{
-          overflowX: "auto",
-          whiteSpace: "nowrap",
-          overflowX: "hidden",
-          scrollBehavior: "smooth",
-        }}
-        ref={scrollRef}
-      >
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
-          <div
-            key={value}
-            style={{ display: "inline-block", marginRight: "5px" }}
-          >
-            <Image
-              src={fareimage}
-              alt="logo"
-              style={{ width: "350px", height: "500px" }}
-            />
+
+      <div className="flex m-5 gap-5 relative">
+        <div className="overflow-auto w-[70%] h-screen pr-5 sticky top-0">
+          <div className="flex items-center m-5 gap-5 bg-slate-100">
+            <div className="w-[4px] h-10 bg-black"></div>
+            <h1 className="text-2xl">Fitness</h1>
           </div>
-        ))}
-      </div>
-      <div
-        className="cursor-pointer hover:bg-gray-200 h-[40px] w-[40px] flex items-center justify-center"
-        onClick={() => handleScroll("next")}
-      >
-        <ArrowForwardIos />
-      </div>
-    </div> 
-       
-      </Box>
 
-      <Grid container spacing={2}  >
-        <Grid item xs={12} sm={8} md={9}>
-          <Box sx={{ mb: 3 }}>
-            <Typography variant="h4" sx={{ mb: 2 }}>
-              Fitness
-            </Typography>
+          <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} md={4}>
-                <Image src={fareimage} alt="logo" width={300} height={200} />
+              <Grid item xs={6}>
+                <Image src={fareimage} alt="logo" />
               </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Image src={fareimage} alt="logo" width={300} height={200} />
+
+              <Grid item xs={6}>
+                <Image src={fareimage} alt="logo" />
               </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Image src={fareimage} alt="logo" width={300} height={200} />
+              <Grid item xs={4}>
+                <Image src={fareimage} alt="logo" />
+              </Grid>
+              <Grid item xs={4}>
+                <Image src={fareimage} alt="logo" />
+              </Grid>
+              <Grid item xs={4}>
+                <Image src={fareimage} alt="logo" />
               </Grid>
             </Grid>
           </Box>
 
-          <Box sx={{ mb: 3 }}>
-            <Typography variant="h4" sx={{ mb: 2 }}>
-              Entertainment
-            </Typography>
-            <Grid container spacing={2}>
-              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
-                <Grid item xs={12} key={value}>
-                 <CategoryCard params={"dfgdfg"}/>
-                </Grid>
-              ))}
-            </Grid>
+          <Box sx={{ flexGrow: 1 }}>
+            <div className="flex items-center m-5 gap-5 bg-slate-100">
+              <div className="w-[4px] h-10 bg-black"></div>
+              <h1 className="text-2xl">Entertainment</h1>
+            </div>
+          
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+              <Card key={value} sx={{ maxWidth: '100%', boxShadow: 'none', border: 'none' }}>
+                <CardActionArea sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, border: 'none', boxShadow: 'none' }}>
+                  <Box sx={{ flex: '0 0 auto', width: { xs: '100%', md: '250px' }, height: { xs: 'auto', md: '170px' } }}>
+                    <Image src={fareimage} alt="category image" layout="responsive" width={250} height={170} />
+                  </Box>
+                  <CardContent sx={{ flex: '1 1 auto', p: 2 }}>
+                    <Typography gutterBottom variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+                      Prioritise Comfort And Health With Shoes For Plantar Fasciitis
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Prioritise Comfort And Health With Shoes For Plantar Fasciitis. Prioritise Comfort And Health With Shoes For Plantar Fasciitis. Prioritise Comfort And Health With Shoes For Plantar Fasciitis.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
           </Box>
-        </Grid>
+          
+        </div>
 
-        <Grid item xs={12} sm={4} md={3}>
-          <RightBar />
-          <AdvertisementBanner />
-        </Grid>
-      </Grid>
-    </Box>
+        <div className="sticky top-0 right-0 h-screen overflow-y-auto w-[30%]" ref={rightBarRef}>
+        <RightBar />
+        <AdvertisementBanner />
+      </div>
+      </div>
+    </div>
   );
 }
