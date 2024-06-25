a72a56d5434fc672df95971eb5e6e9f279e5cd8b
Harshit <rajputharshit48@gmail.com>
2024-06-25 20:33:17 +0530

    edit grid

--- app/page.js
+++ app/page.js
@@ -1,12 +1,39 @@
 "use client";
 
-import { Grid, Paper, Box, styled, Card, CardActionArea, Typography, CardContent } from "@mui/material";
+import {
+  Grid,
+  Paper,
+  Button,
+  Box,
+  styled,
+  Card,
+  CardActionArea,
+  Typography,
+  CardContent,
+} from "@mui/material";
 import Image from "next/image";
 import { useState, useRef } from "react";
 import fareimage from "../public/fareimage.jpg";
 import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 import RightBar from "@/components/Category/RightBar/RightBar";
 import AdvertisementBanner from "@/components/Category/RightBar/advertisementBanner";
+// import  models  from "@/public/images";
+import {
+  fitness1,
+  fitness2,
+  fitness3,
+  fitness4,
+  fitness5,
+  logo1,
+  logo2,
+  logo3,
+  logo4,
+  model1,
+  model2,
+  model3,
+  model4,
+  model5,
+} from "@/public/images";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -18,26 +45,25 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Home() {
   const [spacing, setSpacing] = useState(2);
-  const mainContentRef = useRef(null);
-  const rightBarRef = useRef(null);
+  const scrollRef = useRef(null);
 
   const handleScroll = (direction) => {
-    if (mainContentRef.current) {
-      const { scrollLeft, clientWidth } = mainContentRef.current;
+    if (scrollRef.current) {
+      const { scrollLeft, clientWidth } = scrollRef.current;
       const scrollAmount = clientWidth / 2; // Adjust as needed
       if (direction === "next") {
-        mainContentRef.current.scrollLeft = scrollLeft + scrollAmount;
+        scrollRef.current.scrollLeft = scrollLeft + scrollAmount;
       } else {
-        mainContentRef.current.scrollLeft = scrollLeft - scrollAmount;
+        scrollRef.current.scrollLeft = scrollLeft - scrollAmount;
       }
     }
   };
 
   return (
-    <div className="relative min-h-screen">
-      <div className="flex items-center m-5 gap-5">
+    <Box sx={{ p: 2 }}>
+      <div className="flex items-center m-5 gap-5 ">
         <div
-          className="cursor-pointer hover:bg-gray-200 h-[40px] w-[40px] flex items-center justify-center"
+          className="cursor-pointer hover:bg-gray-200 h-[40px] w-[40px] flex items-center justify-center" // Apply hover background color directly
           onClick={() => handleScroll("prev")}
         >
           <ArrowBackIos />
@@ -49,20 +75,22 @@ export default function Home() {
             overflowX: "hidden",
             scrollBehavior: "smooth",
           }}
-          ref={mainContentRef}
+          ref={scrollRef}
         >
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
-            <div
-              key={value}
-              style={{ display: "inline-block", marginRight: "5px" }}
-            >
-              <Image
-                src={fareimage}
-                alt="logo"
-                style={{ width: "350px", height: "500px" }}
-              />
-            </div>
-          ))}
+          {[model1, model4, model5, model1, model4, model5, model2, model3].map(
+            (value) => (
+              <div
+                key={value}
+                style={{ display: "inline-block", marginRight: "5px" }}
+              >
+                <Image
+                  src={value}
+                  alt="logo"
+                  style={{ width: "350px", height: "500px" }}
+                />
+              </div>
+            )
+          )}
         </div>
         <div
           className="cursor-pointer hover:bg-gray-200 h-[40px] w-[40px] flex items-center justify-center"
@@ -72,66 +100,140 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex m-5 gap-5 relative">
-        <div className="overflow-auto w-[70%] h-screen pr-5 sticky top-0">
-          <div className="flex items-center m-5 gap-5 bg-slate-100">
+      <div className="relative flex flex-col md:flex-row">
+        <div className="sticky top-0  md:h-[800px] overflow-y-hidden   flex-1">
+          <div className="flex items-center mb-5 gap-5 bg-[#1313132c]">
             <div className="w-[4px] h-10 bg-black"></div>
             <h1 className="text-2xl">Fitness</h1>
           </div>
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={9}
+            style={{ height: "100%", overflowY: "auto" }}
+          >
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Image
+                    src={fitness1}
+                    alt="logo"
+                    layout="responsive"
+                    width={300}
+                    height={200}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Image
+                    src={fitness2}
+                    alt="logo"
+                    layout="responsive"
+                    width={300}
+                    height={200}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Image
+                    src={fitness4}
+                    alt="logo"
+                    layout="responsive"
+                    width={300}
+                    height={200}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Image
+                    src={fitness3}
+                    alt="logo"
+                    layout="responsive"
+                    width={300}
+                    height={200}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Image
+                    src={fitness5}
+                    alt="logo"
+                    layout="responsive"
+                    width={300}
+                    height={200}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
 
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <Image src={fareimage} alt="logo" />
+            <Box sx={{ mb: 3 }}>
+              <div className="flex items-center mb-10 gap-5 bg-[#1313132c]">
+                <div className="w-[4px] h-10 bg-black"></div>
+                <h1 className="text-2xl">Entertainment</h1>
+              </div>
+              <Grid container spacing={2}>
+                {[model1, fitness2, logo3, model4, logo1, logo2, logo3, logo4].map(
+                  (value) => (
+                    <Card
+                      key={value}
+                      sx={{
+                        maxWidth: "100%",
+                        boxShadow: "none",
+                        border: "none",
+                      }}
+                    >
+                      <CardActionArea
+                        sx={{
+                          display: "flex",
+                          flexDirection: { xs: "column", md: "row" },
+                          border: "none",
+                          boxShadow: "none",
+                        }}
+                      >
+                        <Box
+                          sx={{
+                            flex: "0 0 auto",
+                            width: { xs: "100%", md: "250px" },
+                            height: { xs: "auto", md: "170px" },
+                          }}
+                        >
+                          <Image
+                            src={value}
+                            alt="category image"
+                            layout="responsive"
+                            width={250}
+                            height={170}
+                          />
+                        </Box>
+                        <CardContent sx={{ flex: "1 1 auto", p: 2 }}>
+                          <Typography
+                            gutterBottom
+                            variant="h6"
+                            component="div"
+                            sx={{ fontWeight: "bold" }}
+                          >
+                            Prioritise Comfort And Health With Shoes For Plantar
+                            Fasciitis
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            Prioritise Comfort And Health With Shoes For Plantar
+                            Fasciitis. Prioritise Comfort And Health With Shoes
+                            For Plantar Fasciitis. Prioritise Comfort And Health
+                            With Shoes For Plantar Fasciitis.
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  )
+                )}
               </Grid>
-
-              <Grid item xs={6}>
-                <Image src={fareimage} alt="logo" />
-              </Grid>
-              <Grid item xs={4}>
-                <Image src={fareimage} alt="logo" />
-              </Grid>
-              <Grid item xs={4}>
-                <Image src={fareimage} alt="logo" />
-              </Grid>
-              <Grid item xs={4}>
-                <Image src={fareimage} alt="logo" />
-              </Grid>
-            </Grid>
-          </Box>
-
-          <Box sx={{ flexGrow: 1 }}>
-            <div className="flex items-center m-5 gap-5 bg-slate-100">
-              <div className="w-[4px] h-10 bg-black"></div>
-              <h1 className="text-2xl">Entertainment</h1>
-            </div>
-          
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
-              <Card key={value} sx={{ maxWidth: '100%', boxShadow: 'none', border: 'none' }}>
-                <CardActionArea sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, border: 'none', boxShadow: 'none' }}>
-                  <Box sx={{ flex: '0 0 auto', width: { xs: '100%', md: '250px' }, height: { xs: 'auto', md: '170px' } }}>
-                    <Image src={fareimage} alt="category image" layout="responsive" width={250} height={170} />
-                  </Box>
-                  <CardContent sx={{ flex: '1 1 auto', p: 2 }}>
-                    <Typography gutterBottom variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
-                      Prioritise Comfort And Health With Shoes For Plantar Fasciitis
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      Prioritise Comfort And Health With Shoes For Plantar Fasciitis. Prioritise Comfort And Health With Shoes For Plantar Fasciitis. Prioritise Comfort And Health With Shoes For Plantar Fasciitis.
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            ))}
-          </Box>
-          
+            </Box>
+          </Grid>
         </div>
+        <div className="sticky top-0  h-auto md:h-[100%] w-full md:w-[30%]  overflow-hidden">
+          <RightBar />
+          <AdvertisementBanner />
+        </div>
+      </div>
 
-        <div className="sticky top-0 right-0 h-screen overflow-y-auto w-[30%]" ref={rightBarRef}>
-        <RightBar />
-        <AdvertisementBanner />
-      </div>
-      </div>
-    </div>
+      <div className="h-screen bg-blue-300"></div>
+    </Box>
   );
 }
--- components/Category/RightBar/advertisementBanner.js
+++ components/Category/RightBar/advertisementBanner.js
@@ -2,14 +2,15 @@ import { Box } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 import fareimage from '@/public/fareimage.jpg'
+import {logo1,logo2,logo3,logo4} from '../../../public/images.js'
 
 const AdvertisementBanner = () => {
   return (
     <Box sx={{width:"100%",height:"auto" ,paddingTop:"15px"}}>
-    {[0, 1, 2,3,4,5].map((item, index) => ( // Only iterate over 3 elements
-      <Box key={index} sx={{width:"100%",height:"auto" ,paddingTop:"15px"}}>
+    {[logo1,logo2,logo3,logo4,logo1,logo2,logo3,logo4].map((value) => ( // Only iterate over 3 elements
+      <Box sx={{width:"100%",height:"auto" ,paddingTop:"25px" , marginLeft:"10px"}}>
         <Image
-          src={fareimage}
+          src={value}
           alt="logo"
           style={{ width: "350px", height: "400px" }}
         />
